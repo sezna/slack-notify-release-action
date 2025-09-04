cbc6e52f9f3e142dd26c235f034be8f95c809cfb
sezna <alehansen@microsoft.com>
2025-09-03 18:42:10 -0700

    update

--- index.js
+++ index.js
@@ -3,6 +3,29 @@ const github = require('@actions/github')
 const https = require('https')
 const simpleGit = require('simple-git');
 
+const translations = {
+  italian: {
+    pretext: projectName => `Rilasciata la nuova versione di ${projectName}!`,
+    text: changelogUrl => `Changelog disponibile qua: ${changelogUrl}`
+  },
+  russian: {
+    pretext: projectName => `Выпущена новая версия ${projectName}!`,
+    text: changelogUrl => `Список изменений доступен здесь: ${changelogUrl}`
+  },
+  polish: {
+    pretext: projectName => `Wydano nową wersję ${projectName}!`,
+    text: changelogUrl => `Lista zmian dostępna tutaj: ${changelogUrl}`
+  },
+  french: {
+    pretext: projectName => `Nouvelle version de ${projectName} publiée!`,
+    text: changelogUrl => `Journal des modifications disponible ici: ${changelogUrl}`
+  },
+  spanish: {
+    pretext: projectName => `¡Nueva versión de ${projectName} lanzada!`,
+    text: changelogUrl => `Registro de cambios disponible aquí: ${changelogUrl}`
+  }
+}
+
 const main = async () => {
   const currentRepoGit = simpleGit();
   const tags = (await currentRepoGit.tags({'--sort' : 'taggerdate'})).all
@@ -15,13 +38,17 @@ const main = async () => {
   const channelId = core.getInput('channel_id')
   const projectName = core.getInput('project_name')
   
+  const languages = Object.keys(translations)
+  const randomLanguage = languages[Math.floor(Math.random() * languages.length)]
+  const selectedTranslation = translations[randomLanguage]
+  
   const payload = JSON.stringify({
     channel: channelId,
     token: slackToken,
     attachments: [
       {
-        pretext : `Rilasciata la nuova versione di ${projectName}: ${version}!`,
-        text : `Changelog disponibile qua: ${changelogUrl}`,
+        pretext : selectedTranslation.pretext(projectName),
+        text : selectedTranslation.text(changelogUrl),
       },
     ],
   })
